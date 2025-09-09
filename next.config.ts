23c3c7b8b1644dee230cf16bfc0daf3afbc6bc65
Ramesh Bojanapu <84425972+Ramesh-Bojanapu-1011@users.noreply.github.com>
2025-09-09 15:27:44 +0530

    final commit

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,28 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   devIndicators: false,
   allowedDevOrigins: ["*"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.svgrepo.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.freepik.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "2793236.fs1.hubspotusercontent-na1.net",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
